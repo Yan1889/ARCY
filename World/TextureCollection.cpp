73c49b9076710f67b665805b1657a9ae1c65e646
Yan1889 <yan.amin256@gmail.com>
2025-07-07 19:13:15 +0200

    3. option im city laden hinzugefügt haha

--- World/TextureCollection.cpp
+++ World/TextureCollection.cpp
@@ -8,8 +8,11 @@ Texture2D TextureCollection::city = {};
 
 void TextureCollection::LoadAll() {
     // Use 2nd option for web, else 1st
-    //city = LoadTexture("src/images/city.png");
-    city = LoadTexture("city.png");
+    // city = LoadTexture("src/images/city.png");
+    // city = LoadTexture("city.png");
+
+    // was ist hier los haha
+    city = LoadTexture("images/city.png");
 }
 
 void TextureCollection::UnloadAll() {
